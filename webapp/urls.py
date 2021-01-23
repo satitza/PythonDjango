d692a67bc3ce9f5d8959cf56614fe3864783eed6
Satit Porntepanon <st_satitza@hotmail.com>
2021-01-23 22:09:50 +0700

    add templates and static folder

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from webapp import views
 
 urlpatterns = [
-    path('', views.Index, name='index')
+    path('', views.Index, name='index'),
+    path('add/<int:id>/<str:name>', views.Add, name='add'),
+    path('page', views.Page, name='page')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -7,3 +7,15 @@ from django.http import HttpResponse, JsonResponse
 
 def Index(request):
     return HttpResponse('Index')
+
+
+def Add(request, id, name):
+    json_user = {
+        'id': id,
+        'name': name
+    }
+    return JsonResponse(json_user)
+
+
+def Page(request):
+    return render(request, 'index.html')
